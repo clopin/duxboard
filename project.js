b54f9d8f880336ea8adaee07a8ad6980c9ba4fa7
Kevin De Mulder <kdemulder@gmail.com>
2015-03-12 16:28:28 +0100

    Projects refactored as well

---
+++ project.js
@@ -0,0 +1,25 @@
+// Set the globals that are required
+//var resource = this;
+mongoose = require('mongoose');
+var projectSchema = mongoose.Schema({name: String, counter: Number});
+var projectModel = mongoose.model('project', projectSchema);
+
+var getProjectList = function getProjectList(res) {
+    projectModel.find().lean().exec(function (err, projectlist) {
+        res.render('projects', {title: 'Projects', projects: JSON.parse(JSON.stringify(projectlist))});
+    });
+};
+
+var insertProject = function insertProject(input, callback) {
+    var newproject = new projectModel(input);
+    newproject.save(function (err) {
+        if (err) {
+            res.writeHead(500, {'content-type': 'text/plain'});
+            res.end('An error occurred');
+        };
+        callback();
+    });
+};
+
+module.exports.getProjectList = getProjectList;
+module.exports.insertProject = insertProject;
--- routes/projects.js
+++ routes/projects.js
@@ -1,38 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-// Set the globals that are required
-mongoose  = require('mongoose');
-var projectSchema = mongoose.Schema({name: String, counter: Number});
-var projectModel = mongoose.model('project', projectSchema);
+var project = require('../project.js');
 
-// Connect Mongoose to the db already
-// mongoose.connect(process.env.MONGOLAB_URI);
 
-function getProjectList(req, res) {
-	projectModel.find().lean().exec(function (err, projectlist) {
-		res.render('projects', {title: 'Projects', projects: JSON.parse(JSON.stringify(projectlist))});
-	});
-}
-
-/* GET project listing. */
+/* GET projects listing. */
 router.get('/', function(req, res, next) {
-	getProjectList(req, res)
+    project.getProjectList(res)
 });
 
 router.get('/new', function(req, res, next) {
-  res.render('newproject', {title: 'New Project'})
-}); 
+    res.render('newproject', {title: 'New Project'})
+});
 
 router.post('/', function(req,res){
-	var newproject = new projectModel({ name: req.body.name, counter: req.body.counter });
-	newproject.save(function (err) {
-		if (err) {
-	  		res.writeHead(500, {'content-type': 'text/plain'});
-	    	res.end('An error occurred');
-		};
-		getProjectList(req, res)
-	});
+    project.insertProject({ name: req.body.name, counter: req.body.counter }, function () {project.getProjectList(res)});
 });
 
 module.exports = router;

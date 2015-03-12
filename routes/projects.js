c8d25d0b4d996749ad44fb9546d254e56bd027a2
Kevin De Mulder <kdemulder@gmail.com>
2015-03-12 13:36:17 +0100

    Database handling now in separate file Create and listing of projects included

---
+++ routes/projects.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+// Set the globals that are required
+mongoose = require('../db.js');
+var projectSchema = mongoose.Schema({name: String, counter: Number});
+var projectModel = mongoose.model('project', projectSchema);
+
+// Connect Mongoose to the db already
+// mongoose.connect(process.env.MONGOLAB_URI);
+
+function getProjectList(req, res) {
+	projectModel.find().lean().exec(function (err, projectlist) {
+		res.render('projects', {title: 'Projects', projects: JSON.parse(JSON.stringify(projectlist))});
+	});
+}
+
+/* GET project listing. */
+router.get('/', function(req, res, next) {
+	getProjectList(req, res)
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('newproject', {title: 'New Project'})
+}); 
+
+router.post('/', function(req,res){
+	var newproject = new projectModel({ name: req.body.name, counter: req.body.counter });
+	newproject.save(function (err) {
+		if (err) {
+	  		res.writeHead(500, {'content-type': 'text/plain'});
+	    	res.end('An error occurred');
+		};
+		getProjectList(req, res)
+	});
+});
+
+module.exports = router;
--- routes/resources.js
+++ routes/resources.js
@@ -1,29 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var config_reader = require('yaml-config');
-// Read in the configuration files that we have
-//var config_db = config_reader.readConfig('../config/database.yml');
 
 // Set the globals that are required
-mongoose = require('mongoose');
+mongoose = require('../db.js');
 var resourceSchema = mongoose.Schema({name: String, counter: Number});
 var resourceModel = mongoose.model('resource', resourceSchema);
 
 // Connect Mongoose to the db already
-mongoose.connect(process.env.MONGOLAB_URI);
+// mongoose.connect(process.env.MONGOLAB_URI);
 
 function getResourceList(req, res) {
 	resourceModel.find().lean().exec(function (err, resourcelist) {
-		//console.log(JSON.stringify(resourcelist));
 		res.render('resources', {title: 'Resources', resources: JSON.parse(JSON.stringify(resourcelist))});
 	});
 }
 
 /* GET resources listing. */
 router.get('/', function(req, res, next) {
-	//db.once('open', function (callback) {
-		getResourceList(req, res)
-	//});
+	getResourceList(req, res)
 });
 
 router.get('/new', function(req, res, next) {
@@ -31,17 +25,14 @@ router.get('/new', function(req, res, next) {
 }); 
 
 router.post('/', function(req,res){
-	//db.once('open', function (callback) {
-		var newresource = new resourceModel({ name: req.body.name, counter: req.body.counter });
-		newresource.save(function (err) {
-			if (err) {
-		  		res.writeHead(500, {'content-type': 'text/plain'});
-		    	res.end('An error occurred');
-			};
-			getResourceList(req, res)
-		});
-		
-	//});
+	var newresource = new resourceModel({ name: req.body.name, counter: req.body.counter });
+	newresource.save(function (err) {
+		if (err) {
+	  		res.writeHead(500, {'content-type': 'text/plain'});
+	    	res.end('An error occurred');
+		};
+		getResourceList(req, res)
+	});
 });
 
 module.exports = router;
